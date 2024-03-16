8e0c80bb292437f09ae2bdaf483307df016797f3
mihyun <aleodrnfl@naver.com>
2024-03-17 02:53:59 +0900

    Metric - connector 구현 완료

--- src/spaceone/inventory/connector/management/metric_connector.py
+++ src/spaceone/inventory/connector/management/metric_connector.py
@@ -12,10 +12,11 @@ class MetricConnector(NaverCloudConnector):
         super().__init__(**kwargs)
 
     def get_metric_list(self):
-        # print("metric client: ", self.metric_client)
+        # print("metric client: ", self.metric_client) #이 코드는 클라이언트 정보를 보여주는 코드 Response201
         # print(self.metric_client.text)
-        response = json.dumps(self.metric_client.json(), indent=4)
+        # response = json.dumps(self.metric_client.json(), indent=4) #json으로 덤프해서 그런가??
         # print("response", response)
+        response = self.metric_client.json()
         return response
 
-##
+## response = { 'metrics' : [~~~], 'prodKey': "22344" }
